3cd4935fc894ba025b0b647b4e5c9f08dcbb52b3
porphiros <darkrisingforce@gmail.com>
2018-07-22 11:41:41 +0300

    add settings functionality:
    -add binding to preference by applying a change listener

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.quakereport;
 
+import android.content.SharedPreferences;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,12 +18,32 @@ public class SettingsActivity extends AppCompatActivity {
 
     }
 
-    public static class EarthquakePreferenceFragment extends PreferenceFragment{
+    public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener{
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             //add the xml layout of the Preferences to the fragment
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
+
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            //after the user change the settings we receive the change in newValue object
+            //we convert to string since we know that it's a numerical string value we setup
+            String value = newValue.toString();
+            preference.setSummary(value);
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
